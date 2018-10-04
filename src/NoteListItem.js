5541501e501e7714b8db84ef7a2f457f7bb119f0
Rikki Guy <git@rikkiguy.me.uk>
2018-10-04 12:15:20 +0100

    Make the list have working links

--- src/NoteListItem.js
+++ src/NoteListItem.js
@@ -1,16 +1,19 @@
 import type {Note} from "./NoteDB";
 import * as React from "react";
 import glamorous from "glamorous";
+import {Link} from "react-router-dom";
 
 type NoteListItemProps = {
     note: Note,
 };
 
 const NoteListItem = ({note}: NoteListItemProps) => (
-    <div>
-        <div>{note.title}</div>
-        <div>{note.lastModified}</div>
-    </div>
+    <Link to={`/note/${note.id}`} style={{ textDecoration: 'none' }}>
+        <div>
+            <div>{note.title}</div>
+            <div>{note.lastModified}</div>
+        </div>
+    </Link>
 );
 
 export default NoteListItem;
